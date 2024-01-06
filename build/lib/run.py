cf8f5620687ea1f4d78d3abf1e0edfe0ff48ed4f
jhunu.fernandes <jhunu.fernandes@gmail.com>
2024-01-06 20:02:51 -0300

    fix gitignore cache

--- build/lib/run.py
+++
@@ -1,9 +0,0 @@
-from os import getenv
-
-from spec_example import EXAMPLE_SPEC
-from sthali_crud import AppSpecification, SthaliCRUD, load_and_parse_spec_file
-
-spec_file_path = getenv("SPEC_FILE_PATH")
-spec_dict = load_and_parse_spec_file(spec_file_path) if spec_file_path else EXAMPLE_SPEC
-client = SthaliCRUD(AppSpecification(**spec_dict))
-app = client.app
